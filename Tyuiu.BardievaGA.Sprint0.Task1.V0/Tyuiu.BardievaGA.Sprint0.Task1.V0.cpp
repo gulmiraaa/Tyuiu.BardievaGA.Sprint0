fff0910433175558769adb929bcd759b20b9e81a
Tyuiu.BardievaGA@outlook.com <bardievaga@mail.ru>
2023-09-20 21:11:42 +0500

    Sprint0.Task1

---
+++ Tyuiu.BardievaGA.Sprint0.Task1.V0/Tyuiu.BardievaGA.Sprint0.Task1.V0.cpp
@@ -0,0 +1,18 @@
+// Tyuiu.BardievaGA.Sprint0.Task1.V0.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    cout << "Hello World!\n";
+    cout << "Введите ФИО : ";
+    string a;
+    cin >> a;
+    int v;
+    cin >> v;
+    cout << "Возраст = " << v;
+    return 0; 
+}
+
