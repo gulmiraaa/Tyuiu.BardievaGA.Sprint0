727dd8ba36124391ed22b764c3298a355b6d4f6b
Tyuiu.BardievaGA@outlook.com <bardievaga@mail.ru>
2023-09-29 19:37:13 +0500

    Task3

---
+++ Tyuiu.BardievaGA.Sprint0.Task3.V0.Test/DataServiseTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.BardievaGA.Sprint0.Task3.V0.Lib;
+
+namespace Tyuiu.BardievaGA.Sprint0.Task3.V0.Test
+{
+    [TestClass]
+    public class DataServiseTest
+    {
+        [TestMethod]
+        public void ChekcedValid()
+        {
+            Assert.AreEqual(10, DataService.Sum(5, 5));
+        }
+    }
+}
---
+++ Tyuiu.BardievaGA.Sprint0.Task3.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.BardievaGA.Sprint0.Task3.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.BardievaGA.Sprint0.Task3.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("fefbed32-01b5-414d-bf7a-f58fb5cb2f63")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
